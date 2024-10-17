dfa6f3f80ab53b908f95d529d23ee00f077984f2
vdrisya <vdrisya721@gmail.com>
2024-10-17 19:54:33 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,29 +6,32 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import { Link } from 'react-router-dom';
-export const Navbar = () => {
+
+
+const Nav = () => {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            {/* <MenuIcon /> */}
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            user app
-          </Typography>
-          <Link to={'/'}><Button color="inherit">Home</Button></Link>
-          <Link to={'/add'}><Button color="inherit">Add</Button></Link>
-          
-        </Toolbar>
-      </AppBar>
-    </Box>      
+    <><Box sx={{ flexGrow: 1, }}>
+    <AppBar position="static"sx={{ backgroundColor:'#3C3D37' }}>
+      <Toolbar>
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 2 }}
+        >
+       
+        </IconButton>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+             KaRT
+        </Typography>
+        <Link to={'/'}><Button  variant='contained' color="secondary" sx={{ margin:3,backgroundColor:'#000000' }} >Home</Button></Link>
+        <Link to={'/add'}><Button color="secondary" variant='contained' sx={{ backgroundColor:'#000000' }}>Cart</Button></Link>
+      </Toolbar>
+    </AppBar>
+  </Box>
+    </>
   )
 }
-export default Navbar;
+
+export default Nav
